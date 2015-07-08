dfd953746a3b4879e6b4eadfb99147a0ee9957ee
kksrini89 <kksrini89@gmail.com>
2015-07-09 00:12:27 +0530

    Initial Check-In

---
+++ Sample/App/Customer/Controllers/CustomerController.js
@@ -0,0 +1,23 @@
+﻿//(function () {
+//    angular.module('MultilingualApp').controller('CustomerController', ['$scope', '$translatePartialLoader', '$translate', function ($scope, $translatePartialLoader, $translate) {
+//        $translatePartialLoader.addPart('Customer');
+//        var isPartAvailable = $translatePartialLoader.isPartAvailable('Customer');
+//        if (isPartAvailable) {
+//            $translate('Main_CustomerContent').then(function (data) {
+//                $scope.content = data;
+//            });
+//        }
+//    }]);
+//})();
+(function () {
+    angular.module('MultilingualApp').controller('CustomerController', ['$rootScope', '$translatePartialLoader', '$translate', function ($rootScope, $translatePartialLoader, $translate) {
+        $translatePartialLoader.addPart('Customer');
+        var isPartAvailable = $translatePartialLoader.isPartAvailable('Main');
+        if (isPartAvailable) {
+            $translate('Main_CustomerHeading').then(function (data) {
+                $rootScope.PageSubTitle = data;
+                console.log($rootScope.PageSubTitle);
+            });
+        }
+    }]);
+})();
---
+++ Sample/App/Retailer/Controllers/RetailerController.js
@@ -0,0 +1,24 @@
+﻿//(function () {
+//    angular.module('MultilingualApp').controller('RetailerController', ['$scope', '$translatePartialLoader', '$translate', function ($scope, $translatePartialLoader, $translate) {
+//        $translatePartialLoader.addPart('Retailer');
+//        var isPartAvailable = $translatePartialLoader.isPartAvailable('Retailer');
+//        if (isPartAvailable) {
+//            $translate('Main_RetailerContent').then(function (translatedValue) {
+//                $scope.content = translatedValue;
+//                console.log($scope.content);
+//            });
+//        }
+//    }]);
+//})();
+(function () {
+    angular.module('MultilingualApp').controller('RetailerController', ['$rootScope', '$translatePartialLoader', '$translate', function ($rootScope, $translatePartialLoader, $translate) {
+        $translatePartialLoader.addPart('Retailer');
+        var isPartAvailable = $translatePartialLoader.isPartAvailable('Main');
+        if (isPartAvailable) {
+            $translate('Main_RetailerHeading').then(function (translatedValue) {
+                $rootScope.PageSubTitle = translatedValue;
+                console.log($rootScope.PageSubTitle);
+            });
+        }
+    }]);
+})();
